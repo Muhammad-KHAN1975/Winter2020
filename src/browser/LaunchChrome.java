967f8c1f4c2fd630e0b64512409b87b0e16f813f
Muhammad Khan <titaskhan009@gmail.com>
2020-05-09 17:17:29 -0500

    first commit

---
+++ src/browser/LaunchChrome.java
@@ -0,0 +1,23 @@
+package browser;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LaunchChrome {
+	
+	public static void main(String[] args) throws InterruptedException {
+		
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Winter2020Selenium\\LaunchBrowser\\Drivers\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+		
+		driver.get("https://www.selenium.dev/downloads/");
+		driver.manage().window().maximize();
+		Thread.sleep(2000);
+		
+		driver.close();
+	}
+	
+
+}
